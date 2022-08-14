d2e834bc9847f2ef55702ff901929df1309e0234
chirstiankaikai <ckaikai19@gmail.com>
2022-08-13 23:21:48 -0400

    added routes

---
+++ pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView, FlatList } from 'react-native';
+
+import { COLORS, NFTData } from "../constants";
+
+
+
+
+function Home () {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home;
